44b011eb3e33b1c9d26c892921e70fc1253f5440
DESKTOP-R9TM19D\PC <namdp.uet@gmail.com>
2022-10-20 17:03:44 +0700

    up

---
+++ src/index.js
@@ -0,0 +1,4 @@
+$("#login").click(function () {
+    alert("ok")
+    window.location.replace("https://helpex.vn/question/dung-jquery-de-lam-cho-mot-trang-chuyen-huong-url-nhu-the-nao-5629fafc58f8e008a8dec766");
+})
